e511ca4591fbb24076d2f28c956017130f55ba7a
Emilio Del Castillo <emilio.del-castillo@outlook.com>
2021-03-12 22:18:27 +0100

    Reorganise the code

---
+++ Test technique/Main View/Main+Navigation.swift
@@ -0,0 +1,40 @@
+//
+//  Main+Navigation.swift
+//  Test technique
+//
+//  Created by Emilio Del Castillo on 12/03/2021.
+//
+
+import UIKit
+
+extension MainViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        switch segue.identifier {
+        case "showInfo":
+            if let destination = segue.destination as? InfoViewController {
+                
+                // Get the location corresponding to the mark identifier
+                if let location = AppData.shared.locations.first(where: { (loc) -> Bool in
+                    loc.locationId == (sender as! LocationMark).identifier
+                }) {
+                    destination.city = location.city
+                    destination.location = location.location
+                    destination.parameter = location.parameter
+                    destination.value = location.value
+                    destination.unit = location.unit
+                    destination.isMobile = location.isMobile
+                    destination.isAnalysis = location.isAnalysis
+                    destination.entity = location.entity
+                    destination.sensorType = location.sensorType
+                }
+            }
+        case "showFilters":
+            if let destination = segue.destination as? FilterViewController {
+                destination.delegate = self
+            }
+        default:
+            print("Unknown segue")
+        }
+    }
+}
--- Test technique/Main View/MainViewController.swift
+++ Test technique/Main View/MainViewController.swift
@@ -126,40 +126,6 @@ class MainViewController: UIViewController, MainViewDelegate {
             }
         }
     }
-    
-    
-    //MARK: - Navigation
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        switch segue.identifier {
-        case "showInfo":
-            if let destination = segue.destination as? InfoViewController {
-                
-                // Get the location corresponding to the mark identifier
-                if let location = AppData.shared.locations.first(where: { (loc) -> Bool in
-                    loc.locationId == (sender as! LocationMark).identifier
-                }) {
-                    destination.city = location.city
-                    destination.location = location.location
-                    destination.parameter = location.parameter
-                    destination.value = location.value
-                    destination.unit = location.unit
-                    destination.isMobile = location.isMobile
-                    destination.isAnalysis = location.isAnalysis
-                    destination.entity = location.entity
-                    destination.sensorType = location.sensorType
-                }
-            }
-        case "showFilters":
-            if let destination = segue.destination as? FilterViewController {
-                destination.delegate = self
-            }
-        default:
-            print("Unknown segue")
-        }
-    }
-
 }
 
 
